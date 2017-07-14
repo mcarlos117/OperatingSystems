e5a6f9655551107dd104018bb21128fcddc55e39
mcarlos117 <miguelcarlos117@yahoo.com>
2017-07-14 11:19:08 -0500

    Create pslibrary.h

---
+++ assign2/pslibrary.h
@@ -0,0 +1,6 @@
+void part0(char *s1, char *s2);
+void fcfsa(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2);
+void display(char *heading, char *s1, char *s2);
+void sjf (char *s1, char *s2,  int x1, int y1, int z1, int x2, int y2, int z2);
+void psjf (char *s1, char *s2,  int x1, int y1, int z1, int x2, int y2, int z2);
+void rr (char *s1, char *s2,int quantum,  int x1, int y1, int z1, int x2, int y2, int z2);
